fe4de117c96e08a6a8e654b5570cb54263147cf7
wangxinwei <327429246@qq.com>
2026-06-28 21:18:23 +0800

    fix: 移除项目中的冗余代码，移除关于主题切换和暗色模式相关的代码

--- web/src/main.ts
+++ web/src/main.ts
@@ -3,7 +3,5 @@ import { createPinia } from 'pinia'
 import './styles/index.css'
 import App from './App.vue'
 import router from './router'
-import { initTheme } from './utils/theme'
 
-initTheme()
 createApp(App).use(createPinia()).use(router).mount('#app')
